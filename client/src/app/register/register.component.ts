2df12ae4d3762b8151d4942794efe5031c6781b5
Ankita junnarkar <ankita.junnarkar.aj@gmail.com>
2024-09-15 19:24:15 +0530

    End of section 5

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,39 @@
+import { Component, EventEmitter, inject, input, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_service/account.service';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+
+ // usersfromHomeComponent = input.required<any>()
+
+  //old way of sharing data from child component to a parent component using output decorator and event emitter
+  //@Output() cancelRegister =new EventEmitter();
+
+  private accountService = inject(AccountService);
+  cancelRegister = output<boolean>()
+  model: any = {}
+
+  register() {
+    this.accountService.register(this.model).subscribe({
+      next: response =>{
+        console.log(response);
+        this.cancel();
+      },
+      error : error => console.log(error)
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
+
+}
